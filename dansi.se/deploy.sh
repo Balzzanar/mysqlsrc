4847c6937b454a57d33848d201c619e96f4e8ab7
Johan Spann <johan.spann@securitas-direct.com>
2014-04-09 16:04:05 +0200

    Made chaged to deploy.sh

--- dansi.se/deploy.sh
+++ dansi.se/deploy.sh
@@ -29,22 +29,3 @@ mysql -u "$user" -p"$pass" "$db" < pro_dansi_products_get.sql
 
 echo 'PRO: dansi_product_new'
 mysql -u "$user" -p"$pass" "$db" < pro_dansi_product_new.sql
-
-
-
-#-------------------------------------------
-# Place in the 'get_run_deploy.sh'
-# and add 'get_run_deploy.sh' to .gitignore
-
-
-# #!/bin/bash
-#------------------------------------------------
-# Fetches the new code mysqlsrc and deploys it
-# to the mysql instance.
-#
-#------------------------------------------------
-# git fetch --all
-# git reset --hard origin/master
-# ../../set_password.sh
-# deploy.sh
-#-------------------------------------------
